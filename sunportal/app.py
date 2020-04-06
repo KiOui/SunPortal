5aa3fac115202a6e18e590f25d413c2e33e8ea25
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-04-06 18:11:46 +0200

    Pipeline

--- sunportal/app.py
+++ sunportal/app.py
@@ -3,10 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    """
+    Index route.
+
+    :return: render of the index.html template
+    """
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
